a4f2cf292a35eeb06d1b7e252c485c9588d09d0d
DennisDaddy <softteez95@gmail.com>
2017-03-21 15:13:17 +0300

    generate controller Articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+
+  get 'articles/new'
+
+  get 'articles/edit'
+
+  get 'articles/update'
+
+  get 'articles/show'
+
+  get 'articles/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
