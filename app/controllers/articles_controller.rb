464110a517df59d197a0458f0704cb09beded633
DennisDaddy <softteez95@gmail.com>
2017-03-27 15:52:49 +0300

    add find_article action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,6 +32,12 @@ class ArticlesController < ApplicationController
 
   private
 
+  def find_article
+    @article = Article.find(params[:id])
+
+    
+  end
+
   def article_params
     params.require(:article).permit(:title, :content)
     
