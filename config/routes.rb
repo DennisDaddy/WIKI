ade0625bc69a175a396fe30816ad70bc6ea5f109
DennisDaddy <softteez95@gmail.com>
2017-03-28 18:29:42 +0300

    generate controller categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'categories/new'
+
+  get 'categories/show'
+
+  get 'categories/edit'
+
+  get 'categories/update'
+
+  get 'categories/destroy'
+
   devise_for :users
 	root 'articles#index'
 
