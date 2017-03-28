f54cdcbf3d9cf74af330499a03f9743787899b22
DennisDaddy <softteez95@gmail.com>
2017-03-28 08:21:13 +0300

    list categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,13 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
+    if params[:category].blank?
     @articles = Article.all.order("created_at DESC")
+  else
+    @category_id = Category.find_by(name:params[:category]).id
+    @articles = Article.where(category_id: @category_id).order("created_at DESC")
   end
+end
 
   def new
     @article = current_user.articles.build
