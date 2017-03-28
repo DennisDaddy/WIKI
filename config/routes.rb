9bbce57b7d9372ad4a289defdb21e5180fdd9c0e
DennisDaddy <softteez95@gmail.com>
2017-03-28 18:32:24 +0300

    add resources categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'categories/new'
-
-  get 'categories/show'
-
-  get 'categories/edit'
-
-  get 'categories/update'
-
-  get 'categories/destroy'
+  
 
   devise_for :users
 	root 'articles#index'
 
 	resources :articles
+	resources :categories
   
 end
