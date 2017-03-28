ba1ea0f68f704862efcf384a81aa48bf3cd7447e
DennisDaddy <softteez95@gmail.com>
2017-03-28 19:12:06 +0300

    show categories on index page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,66 @@
 class CategoriesController < ApplicationController
-  def new
-  end
-
-  def show
+  def index
+    @categories = Category.all
   end
 
   def edit
   end
 
+  def new
+    @category = Category.new
+    
+  end
+
+  def create
+     @category = Category.new(category_params)
+     if @category.save
+     
+      flash[:success] = "Your category has been saved"
+       redirect_to categories_path
+    else
+      render 'new'
+     end
+    
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @title = @category.name
+    @posts = @category.posts
+  end
+
+  def edit
+     @category = Category.find(params[:id])
+  end
+
   def update
+    @category = Category.find(params[:id])
+
+    if @category.update_attributes(category_params)
+
+       flash[:success] = "Category successfuly updated!!"
+
+
+    
+    redirect_to categories_path
+
+    else
+
+    render 'edit'
+
+    end
   end
 
   def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:success] = "Category successfuly deleted!!"
+    redirect_to request.referrer || root_url
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
